7a5eb9efa80f82d7bb22aa80105b4bc8f2731f74
ThibautG <6084433+ThibautG@users.noreply.github.com>
2025-10-21 16:15:37 +0200

    fix: reverting app state before tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,50 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import {getAllSondes} from "./services/sondesServices.jsx";
 
 function App() {
   const [sondes, setSondes] = useState([])
+  console.log(sondes)
   useEffect(() => {
     const fetchSondes = async () => {
-      const response = await fetch (import.meta.env.VITE_API_URL + `/sondes`)
-      const data = await response.json()
-      setSondes(data.data)
+      const data = await getAllSondes()
+      console.log('data', data)
+      setSondes(data)
     }
     fetchSondes()
   }, []);
 
   return (
     <>
-    <h1>Brief PWA</h1>
-    <ul>
-      {sondes.map((s) => <li>hauteur: {s.haut}</li>)}
-    </ul>
+      <h1 className={'text-3xl font-bold underline'}>Brief PWA</h1>
+      <table className={'table'}>
+        <thead>
+        <tr>
+          <th>_id</th>
+          <th>owner</th>
+          <th>device_id</th>
+          <th>type</th>
+          <th>hauteur</th>
+          <th>volt</th>
+          <th>received_at</th>
+          <th>inserted_at</th>
+        </tr>
+        </thead>
+        <tbody>
+        {sondes.map((s) =>
+          <tr>
+            <td>{s._id}</td>
+            <td>{s.owner}</td>
+            <td>{s.device_id}</td>
+            <td>{s.type}</td>
+            <td>{s.haut}</td>
+            <td>{s.volt}</td>
+            <td>{s.received_at}</td>
+            <td>{s.inserted_at}</td>
+          </tr>
+        )}
+        </tbody>
+      </table>
 
     </>
   )
