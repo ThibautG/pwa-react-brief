ae25e57624679b0dcf264578b28cd871c61d0646
ThibautG <6084433+ThibautG@users.noreply.github.com>
2025-10-21 15:32:51 +0200

    feat(services): move fetch logic in services

---
+++ src/services/sondesServices.jsx
@@ -0,0 +1,7 @@
+const API_URL = import.meta.env.VITE_API_URL
+
+export const getAllSondes = async () => {
+  const response = await fetch (`${API_URL}/sondes`)
+  const data = await response.json()
+  return data.data
+}
