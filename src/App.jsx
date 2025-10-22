dc5c8b2f3fb68b0d0a126998bd77c9fb4862dd47
ThibautG <6084433+ThibautG@users.noreply.github.com>
2025-10-22 14:16:44 +0200

    feat(app): change api route with state modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,47 @@
 import './App.css'
 import DataTab from "./components/DataTab.jsx";
-import React from "react";
+import React, {useState} from "react";
+import {ReloadPrompt} from "./Prompt.jsx";
 
 function App() {
+  const [route, setRoute] = useState('/sondes')
+  console.log(route)
 
   return (
-    <div className="min-h-screen bg-gray-50 p-6">
-      <h1 className="text-3xl font-bold text-center mb-8 text-blue-700 underline">
-        Brief PWA
-      </h1>
-      <h2 className="text-xl font-bold mb-4 text-blue-700">
-        Les sondes
-      </h2>
-      <DataTab apiUrl={'/sondes'}></DataTab>
-      <h2 className="text-xl font-bold mt-8 mb-4 text-blue-700">
-        Les toilettes
-      </h2>
-      <DataTab apiUrl={'/toilettes'}></DataTab>
-    </div>
+    <>
+      <ReloadPrompt></ReloadPrompt>
+
+      <div className="min-h-screen bg-gray-50 p-6">
+        <h1 className="text-3xl font-bold text-center mb-8 text-blue-700 underline">
+          Brief PWA
+        </h1>
+        <nav className={'flex mb-4'}>
+          <ul className={'flex'}>
+            <li className={'mr-3'}>
+              <button onClick={() => setRoute('/sondes')}
+                      className={`px-4 py-2 rounded-lg font-semibold transition 
+                        ${route === "/sondes"
+                          ? "bg-blue-600 text-white shadow-md"
+                          : "bg-white text-blue-700 border border-blue-600 hover:bg-blue-100"}`}
+              >
+                Sondes
+              </button>
+            </li>
+            <li>
+              <button onClick={() => setRoute('/toilettes')}
+                      className={`px-4 py-2 rounded-lg font-semibold transition 
+                        ${route === "/toilettes"
+                        ? "bg-blue-600 text-white shadow-md"
+                        : "bg-white text-blue-700 border border-blue-600 hover:bg-blue-100"}`}
+              >
+                Toilettes
+              </button>
+            </li>
+          </ul>
+        </nav>
+        <DataTab apiUrl={route}></DataTab>
+      </div>
+    </>
   )
 }
 export default App
